6d74b86f83ba017fba1e32f9569b6a6e8e9475ad
Simon Brown <simon.brown@bridgepartners.co.uk>
2019-07-16 21:10:25 +0100

    Removed commented out notes

--- Manage-WirelssNetworks.ps1
+++ Manage-WirelssNetworks.ps1
@@ -61,26 +61,3 @@ Function ConnectTo-WirelessNetwork {
     }
     netsh wlan connect ssid=$ssidName name=$ssidName
 }
-
-<# NOTES
-netsh wlan set profileparameter name=$ssidName SSIDname=$ssidName autoSwitch=no ConnectionMode=auto Randomization=no authentication=WPA2PSK encryption=AES keyType=passphrase keyMaterial=$pwd
-
-netsh wlan set profileparameter /?
-
-Usage: set profileparameter [name=]<string> [[interface=]<string>]
-       [SSIDname=<string>] [ConnectionType=ESS|IBSS] [autoSwitch=yes|no]
-       [ConnectionMode=auto|manual] [nonBroadcast=yes|no]
-       [Randomization=[yes|no|daily]]
-       [authentication=open|shared|WPA|WPA2|WPAPSK|WPA2PSK]
-       [encryption=none|WEP|TKIP|AES] [keyType=networkKey|passphrase]
-       [keyIndex=1-4] [keyMaterial=<string>] [PMKCacheMode=yes|no]
-       [PMKCacheSize=1-255] [PMKCacheTTL=300-86400] [preAuthMode=yes|no]
-       [preAuthThrottle=1-16 [FIPS=yes|no]
-       [useOneX=yes|no] [authMode=machineOrUser|machineOnly|userOnly|guest]
-       [ssoMode=preLogon|postLogon|none] [maxDelay=1-120]
-       [allowDialog=yes|no] [userVLAN=yes|no]
-       [heldPeriod=1-3600] [AuthPeriod=1-3600] [StartPeriod=1-3600]
-       [maxStart=1-100] [maxAuthFailures=1-100] [cacheUserData = yes|no]
-       [cost=default|unrestricted|fixed|variable]
-
-#>
